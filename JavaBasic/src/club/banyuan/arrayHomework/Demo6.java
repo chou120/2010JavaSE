681903ae940439aafe73a28cf251ebf2808cf1e1
周健 <zhoujian@banyuan.club>
2020-11-12 11:17:07 +0800

    作业2

--- JavaBasic/src/club/banyuan/arrayHomework/Demo6.java
+++ JavaBasic/src/club/banyuan/arrayHomework/Demo6.java
@@ -1,5 +1,7 @@
 package club.banyuan.arrayHomework;
 
+import java.util.Arrays;
+
 /**
  * @author sanye
  * @version 1.0
@@ -12,14 +14,40 @@ public class Demo6 {
 
   public  static int [] filterArray(int[]arr){
 
+    //{1, 3, 3, 1, -3, -9, 12, 33};
 
-
-    return null;
+    /**
+     * 1
+     *   3, 3, 1, -3, -9, 12, 33
+     * 0 1  2  3   4   5   6  7
+     */
+    int  key=0;
+    int [] newArray=new int[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      boolean flag=true;  //true 表示没有重复的数字
+      for (int j = i+1; j < arr.length; j++) {
+        if(arr[i]==arr[j]){
+              flag=false;
+              break;
+        }
+      }
+      if(flag){  //false
+        newArray[key++]=arr[i];  //
+      }
+    }
+    //  [3, 1, -3, -9, 12, 33,0,0]
+    int[] infoArray = new int[key];
+    System.out.println("----"+infoArray.length);
+    for (int i = 0; i < infoArray.length; i++) {
+        infoArray[i]=newArray[i];
+    }
+    return infoArray;
   }
 
   public static void main(String[] args) {
+    int[] ints = {1, 3, 3, 1, -3, -9, 12, 33};  // 3  1 -3 -9 12 33
 
-
+    System.out.println(Arrays.toString(filterArray(ints)));
 
   }
 
---
+++ JavaBasic/src/club/banyuan/arrayHomework/Demo8.java
@@ -0,0 +1,48 @@
+package club.banyuan.arrayHomework;
+
+/**
+ * @author sanye
+ * @version 1.0
+ * @date 2020/11/12 11:10 上午
+ */
+public class Demo8 {
+
+  /**
+   * 编写一个方法，输入一个 单位是byte的字节数， 返回一个字符串，后面使用合适的单位进行标识
+   *
+   * @param args
+   */
+
+  public static void main(String[] args) {
+
+    System.out.println(formateByte(1532300001));
+
+
+  }
+
+  public static String formateByte(long bytes) {
+
+    /**
+     * 转换结果如下
+     * 123  => 123.0 B         1000000000B==1000000kB == 1000MB == 1GB
+     * 15323 => 15.323 KB
+     * 15323000 => 15.323 MB
+     * 1532300001 => 1.532300001 GB
+     */
+    String str = "";
+    if (bytes < 1000) {
+      str = str + bytes / 1.0 + "B";
+    } else if (bytes < 1000000) {
+      str = str + bytes / 1000.0 + "KB";
+    } else if (bytes < 1000000000) {
+      str = str + bytes / 1000000.0 + "MB";
+    } else if (bytes < 1000000000000L) {
+      str = str + bytes / 1000000000.0 + "GB";
+    }
+
+    return str;
+
+
+  }
+
+}
